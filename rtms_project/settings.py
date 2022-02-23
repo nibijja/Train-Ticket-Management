e5242da1abcdad3445260c02fdca3efdc55a45c3
Shabbir Ahmed <60581267+Nibijja@users.noreply.github.com>
2022-02-23 13:26:20 +0600

    Update settings.py

--- rtms_project/settings.py
+++ rtms_project/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shabbirahmednibir.bd@gmail.com'
-EMAIL_HOST_PASSWORD = 'T41ck5h0t'
+EMAIL_HOST_USER = 'yourmail@address.com'
+EMAIL_HOST_PASSWORD = 'yourmainpass'
